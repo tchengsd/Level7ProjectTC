b85a54968270274ae3ad9cdfcb6a163fc7ac0786
League Student <league@iMac-16.attlocal.net>
2022-03-10 17:18:16 -0800

    initial

---
+++ src/main/java/org/jointheleague/api/game/Level7ProjectTC/Level7ProjectTcApplication.java
@@ -0,0 +1,13 @@
+package org.jointheleague.api.game.Level7ProjectTC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Level7ProjectTcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Level7ProjectTcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/jointheleague/api/game/Level7ProjectTC/Level7ProjectTcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jointheleague.api.game.Level7ProjectTC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Level7ProjectTcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
