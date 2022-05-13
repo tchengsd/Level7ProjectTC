f9bab94cca65705c2d8909bec72299fd058b2c2f
tchengsd <tchengsd@gmail.com>
2022-05-12 18:25:00 -0700

    Milestone 3

--- src/main/java/org/jointheleague/api/game/Level7ProjectTcApplication.java
+++ src/main/java/org/jointheleague/api/game/Level7ProjectTcApplication.java
@@ -1,4 +1,4 @@
-package org.jointheleague.api.game.Level7ProjectTC;
+package org.jointheleague.api.game;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
