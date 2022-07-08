fe4ff97c409c8cc8d7be4cd85c38cdfe5ee7759a
Tyler Cheng <36546289+tchengsd@users.noreply.github.com>
2022-07-07 18:32:30 -0700

    Update LocRepositoryTest.java
    Milestone 11

--- src/test/java/org/jointheleague/api/unisearch/repository/LocRepositoryTest.java
+++ src/test/java/org/jointheleague/api/unisearch/repository/LocRepositoryTest.java
@@ -6,19 +6,66 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.web.util.UriBuilder;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
+import java.util.function.Function;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 public class LocRepositoryTest {
     private LocRepository repository;
 
     @Mock
-    private WebClient client;
+    WebClient client;
+    
+    @Mock
+    WebClient.RequestHeadersUriSpec requestHeadersUriSpec;
+
+    @Mock
+    WebClient.RequestHeadersSpec requestHeadersSpec;
+
+    @Mock
+    WebClient.ResponseSpec responseSpec;
+
+    @Mock
+    Mono<Result[]> resultMono;
 
     @BeforeEach
     void setup() {
         MockitoAnnotations.initMocks(this);
-        repository = new LocRepository();
+        repository = new LocRepository(client);
+    }
+
+    @Test
+    void whenGetResults_thenReturnResultsArray() {
+        //given
+        String query = "Java";
+        Result result = new Result();
+        result.setCountry("United States");
+        result.setStateProvince("OH");
+        result.setName("Java University in the very interesting state of Ohio");
+        Result[] expectedResults = {result};
+
+        when(client.get())
+                .thenReturn(requestHeadersUriSpec);
+        when(requestHeadersUriSpec.uri((Function<UriBuilder, URI>) any()))
+                .thenReturn(requestHeadersSpec);
+        when(requestHeadersSpec.retrieve())
+                .thenReturn(responseSpec);
+        when(responseSpec.bodyToMono(Result[].class))
+                .thenReturn(resultMono);
+        when(resultMono.block())
+                .thenReturn(expectedResults);
+
+        //when
+        Result[] actualResults = repository.getResults(query);
+        //then
+        assertEquals(expectedResults, actualResults);
     }
 }
+
+
